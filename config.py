e05f1f36791b5f5005729d72962d130e273e8fb8
SaketThite <saketthiteandroid@gmail.com>
2025-07-16 23:05:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Configuration settings for API keys and endpoints
 
-OPENAI_KEY = "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"
-DB_CONN_STRING = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:ecommercecodb.database.windows.net,1433;Database=Payments;Uid=TxReader;Pwd=Saraswati#77;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=300;"
+OPENAI_KEY = "Your key here"
+DB_CONN_STRING = "Your key here"
